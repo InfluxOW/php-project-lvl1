dd4e1eafc20b9960bcf946a7d5a6dd48eb3dad09
InfluxOW <krochak_n@mail.ru>
2019-12-05 18:16:56 +1000

    fifth stage

--- src/Cli.php
+++ src/Cli.php
@@ -5,6 +5,33 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
+const ROUNDS_COUNT = 3;
+
+function game($mission, $getQuestionAndFindCorrectAnswer)
+{
+    line('Welcome to the Brain Game!');
+    PHP_EOL;
+    line($mission);
+    PHP_EOL;
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+
+    for ($roundCounter = 1; $roundCounter <= ROUNDS_COUNT; $roundCounter++) {
+        [$question, $correctAnswer] = $getQuestionAndFindCorrectAnswer();
+        line("Question:, %s!", $question);
+
+        $gameAnswer = prompt('Your answer');
+
+        if ($gameAnswer !== $correctAnswer) {
+            line("'{$gameAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'");
+            line ("Let's try again, {$name}");
+            return;
+        }
+        line("Correct!");
+    }
+    line ("Congratulations, {$name}");
+}
+
 function run()
 {
     line('Welcome to the Brain Game!');
