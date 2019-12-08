a7c44eb601fa4ce04ba0cd11f5ab88ba5cdeb317
InfluxOW <krochak_n@mail.ru>
2019-12-08 15:05:11 +1000

    add choosing random game feature via brain-random

---
+++ src/games/brain-random.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BrainGames\Src\Games\BrainRandom;
+
+use function BrainGames\Src\Games\BrainCalc\calcGame;
+use function BrainGames\Src\Games\BrainEven\evenNumbersGame;
+use function BrainGames\Src\Games\BrainGCD\greatestCommonDivisorGame;
+use function BrainGames\Src\Games\BrainPrime\primeGame;
+use function BrainGames\Src\Games\BrainProgression\progressionGame;
+
+function chooseRandomGame()
+{
+    $randomNumber = rand(0, 4);
+    switch ($randomNumber) {
+        case '0':
+            calcGame();
+            break;
+        case '1':
+            evenNumbersGame();
+            break;
+        case '2':
+            greatestCommonDivisorGame();
+            break;
+        case '3':
+            primeGame();
+            break;
+        case '4':
+            progressionGame();
+            break;
+    }
+}
