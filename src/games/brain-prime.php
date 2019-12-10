1d592b149b1a5c10ee3f23ffde05f3526ab0c417
InfluxOW <krochak_n@mail.ru>
2019-12-10 12:59:01 +1000

    new code for isPrime function

--- src/games/brain-prime.php
+++ src/games/brain-prime.php
@@ -19,8 +19,17 @@ function primeGame()
     game(EXERCISE_DESCRIPTION, $getQuestionAndCorrectAnswer);
 }
 
-function isPrime($question)
+function isPrime($num)
 {
-    $primeNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-    return (in_array($question, $primeNumbers));
+    if ($num <= 1) {
+        return false;
+    }
+
+    for ($start = 2; $start <= $num / 2; $start++) {
+        if ($num % $start === 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
