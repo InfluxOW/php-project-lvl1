7a8f7e8d9645ad57e6f51456774898820d96cecd
InfluxOW <krochak_n@mail.ru>
2019-12-17 12:02:05 +1000

    refactoring

--- src/games/progression.php
+++ src/games/progression.php
@@ -5,16 +5,15 @@ namespace BrainGames\games\progression;
 use function BrainGames\engine\game;
 
 const EXERCISE_DESCRIPTION = 'What number is missing in the progression?';
+const PROGRESSION_LENGTH = 10;
 
 function progressionGame()
 {
-    define('PROGRESSION_LENGTH', 10);
-
     $getQuestionAndCorrectAnswer = function () {
 
         $step = rand(1, 10);
         $progressionStart = rand(0, 100);
-        $originalProgression = generateProgression($step, $progressionStart);
+        $originalProgression = generateProgression($step, $progressionStart, PROGRESSION_LENGTH);
         $progression = $originalProgression;
         $hiddenElementIndex = array_rand($progression);
         $progression[$hiddenElementIndex] = "..";
@@ -28,7 +27,7 @@ function progressionGame()
     game(EXERCISE_DESCRIPTION, $getQuestionAndCorrectAnswer);
 }
 
-function generateProgression($step, $progressionStart)
+function generateProgression($step, $progressionStart, $progressionLength)
 {
     for ($i = 0; $i <= PROGRESSION_LENGTH; $i++) {
         $result[] = $progressionStart + $i * $step;
